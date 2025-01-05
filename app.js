16c14983347462650f3ca69ad0fc24d6cb9e9539
DaChoko <Mailechokoe2@gmail.com>
2025-01-05 14:10:05 +0200

    autosearch functionality is complete

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const searchbar = document.getElementById("search")
+const dataShow = document.getElementById("Data")
+const resultbox = document.getElementById("searchAuto")
 
 document.addEventListener("DOMContentLoaded", function(){
 
-    const searchbar = document.getElementById("search")
-    const dataShow = document.getElementById("Data")
+    
     let myArrayShowTitles = []
     let myArrayMovieTitles = []
 
@@ -10,12 +12,28 @@ document.addEventListener("DOMContentLoaded", function(){
     getMovies(myArrayMovieTitles)
 
     searchbar.addEventListener("input", function(e){
+        let result = []
         const searchTerm = e.target.value.toLowerCase()
-        const filteredResults = searchTerm.filter()
-    })
-    
+        if (searchTerm.length>2){
+            result = myArrayShowTitles.filter((keyword) => {return keyword.toLowerCase().includes(searchTerm.toLowerCase())})
+            console.log(result)
+            displayAutoSearch(result, resultbox)
 
+        }
+    })
 })
+function displayAutoSearch(result, ulbox){
+    const content = result.map((list)=>{
+        return "<li onclick=SelectInput(this)>" + list + "</li>"
+    })
+    ulbox.innerHTML = "<ul>" + content.join("") + "</ul>"
+}
+
+function SelectInput(list){
+    searchbar.value = list.innerHTML
+    resultbox.innerHTML = "";
+
+}
 async function getMovies(array){
     response = await fetch("http://127.0.0.1:5000/movies")
     data = await response.json()
